0ebb6b4f4f28c939df787bfb9beefaf3692832ee
BartekKalinowski1 <bj.kalinowsk@student.uw.edu.pl>
2026-05-25 21:57:48 +0200

    feat: add logging

--- src/quality_checks.py
+++ src/quality_checks.py
@@ -1,5 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def check_quality(df):
-    print("\nData quality check\n")
+    logger.info("Starting data quality checks")
     if not count_null(df):
         return False
     if not check_df_length(df):
@@ -12,48 +17,50 @@ def check_quality(df):
         return False
     if not check_duplicates(df):
         return False
-    print("Quality check passed. Data is ready for loading.")
+    logger.info("Quality check passed. Data is ready for loading.")
     return True
 
 
 def count_null(df):
     count = df.isnull().sum().sum()
     if count > 0:
-        print(f"Quality check failed - found {count} null values.")
+        logger.error(f"Quality check failed - found {count} null values.")
         return False
     return True
 
 
 def check_df_length(df):
     if len(df) == 0:
-        print("Quality check failed - DataFrame is empty.")
+        logger.error("Quality check failed - DataFrame is empty.")
         return False
     return True
 
 
 def check_price_values(df):
     if df["price"].dtype != float:
-        print("Quality check failed - price column is not in float format.")
+        logger.error(
+            "Quality check failed - price column is not in float format.")
         return False
     return True
 
 
 def check_revenue_values(df):
     if df["revenue"].dtype != float:
-        print("Quality check failed - revenue column is not in float format.")
+        logger.error(
+            "Quality check failed - revenue column is not in float format.")
         return False
     return True
 
 
 def check_duplicates(df):
     if df.duplicated().any():
-        print("Quality check failed - duplicate rows found.")
+        logger.error("Quality check failed - duplicate rows found.")
         return False
     return True
 
 
 def check_revenue_column(df):
     if "revenue" not in df.columns:
-        print("Quality check failed - revenue column is missing.")
+        logger.error("Quality check failed - revenue column is missing.")
         return False
     return True

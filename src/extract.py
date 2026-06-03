496eb3004ce8b30ecd53d95efb95f19351bdd787
BartekKalinowski1 <bj.kalinowsk@student.uw.edu.pl>
2026-06-03 22:45:54 +0200

    feat: add logging, parsing error exception and validation if file with data exists

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def extract_data(path):
-    df = pd.read_csv(path)
+    if not os.path.exists(path):
+        logger.error(f"File not found: {path}")
+        raise FileNotFoundError(f"File not found: {path}")
+    try:
+        df = pd.read_csv(path)
+    except pd.errors.ParserError as e:
+        logger.error(f"Error parsing CSV file: {path}. Error:{e}")
+        raise
+    logger.info(f"Data extracted from: {path}. Shape: {df.shape[0]} rows and "
+                f"{df.shape[1]} columns.")
     return df

32e56d747bf27575769510b20da1deed4b8159cd
BartekKalinowski1 <bj.kalinowsk@student.uw.edu.pl>
2026-06-27 11:56:13 +0200

    rewrite load.py to load data into postgreSQL using psycopg2

--- src/load.py
+++ src/load.py
@@ -1,16 +1,70 @@
-import os
 import logging
-from config import OUTPUT_FILE
+from config import DB_CONFIG
+import psycopg2
+from psycopg2.extras import execute_values
 
 logger = logging.getLogger(__name__)
 
 
-def load_data(df, path):
-    os.makedirs(path, exist_ok=True)
-    output_path = os.path.join(path, OUTPUT_FILE)
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+
+def load_raw(df):
+    sql = """
+        INSERT INTO sales.sales_raw(order_id, customer_id, product, quantity,
+              order_date, region, price)
+        VALUES %s
+        ON CONFLICT (order_id) DO NOTHING;
+    """
+
+    rows = [tuple(row) for row in df[["order_id", "customer_id", "product",
+                                      "quantity", "order_date", "region", "price"]].itertuples(index=False)]
     try:
-        df.to_csv(output_path, index=False)
-        logger.info(f"Data loaded to: {output_path}")
+        with get_connection() as conn:
+            with conn.cursor() as curr:
+                execute_values(curr, sql, rows)
+        logger.info(f"Loaded {len(rows)} columns into sales_raw")
     except Exception:
-        logger.exception("Failed to load data")
+        logger.exception("Failed to load raw data")
+        raise
+
+
+def load_clean(df):
+    sql = """
+    INSERT INTO sales.sales_clean(order_id, customer_id, product, 
+                                  quantity, order_date, region, price, revenue, category)
+    VALUES %s
+    ON CONFLICT (order_id) DO NOTHING;
+    """
+    rows = [tuple(row) for row in df[["order_id", "customer_id", "product",
+                                      "quantity", "order_date", "region", "price", "revenue", "category"]].itertuples(index=False)]
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as curr:
+                execute_values(curr, sql, rows)
+        logger.info(f"Loaded {len(rows)} rows into sales_clean.")
+    except Exception:
+        logger.exception("Failed to load clean data.")
+        raise
+
+
+def load_segments():
+    sql = """
+        INSERT INTO sales.customer_segments(customer_id, total_orders, total_revenue, category)
+        SELECT customer_id,
+            COUNT(DISTINCT order_id) as total_orders,
+            SUM(revenue) as total_revenue,
+            category
+        FROM sales.sales_clean
+        GROUP BY customer_id, category
+        ON CONFLICT (customer_id) DO NOTHING;
+    """
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as curr:
+                curr.execute(sql)
+        logger.info("Customer segments loaded.")
+    except Exception:
+        logger.exception("Failed to load customer segments.")
         raise

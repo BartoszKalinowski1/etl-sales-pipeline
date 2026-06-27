eed339093680fdd0ccd09c776be9c5c34afa6438
BartekKalinowski1 <bj.kalinowsk@student.uw.edu.pl>
2026-06-27 11:55:04 +0200

    add data base config

--- config.py
+++ config.py
@@ -6,3 +6,12 @@ OUTPUT_FILE = "cleaned_sales.csv"
 
 REVENUE_THRESHOLD = 100.0
 SAMPLE_DATA_SIZE = 10000
+
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "sales_db",
+    "user": "admin",
+    "password": "admin",
+}
